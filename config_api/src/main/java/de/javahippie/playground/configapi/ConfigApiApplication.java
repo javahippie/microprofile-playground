c70acbf7eecec554c7517fd9d2c7e5d0d1fd9edc
Tim Zöller <tim.zoeller@ilume.de>
2019-04-07 09:45:23 +0200

    Added example for Config API based on Payara

---
+++ config_api/src/main/java/de/javahippie/playground/configapi/ConfigApiApplication.java
@@ -0,0 +1,15 @@
+package de.javahippie.playground.configapi;
+
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+@ApplicationScoped
+
+public class ConfigApiApplication extends Application {
+}
